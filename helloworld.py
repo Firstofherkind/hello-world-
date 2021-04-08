52bf96915c77723696455a333b5ae06cab5a3022
Egwu Jennifer <egwujennifer@github.com>
2021-04-08 22:18:43 +0100

    codes

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+# Day 1 - 30 days of Python.
+print(2 + 3) # addition
+print(2 - 3) # subtraction
+print(2 * 3) # multiplication
+print(3 / 2) # division
+print(3 % 2) # modulo
+print(3 // 2)# floor division
+
+# CHECKING DATA TYPESr
+print(type(10)) # int
+print(type(3.14)) # float
+print(type(1 + 3j)) # complex number
+print(type("Jennifer")) # string
+print(type(["name", 2, 5, "Jeniinfer"])) # list
+print(type({"name": "Jennifer", "Age":13,}))# dictiotnary
+
+
